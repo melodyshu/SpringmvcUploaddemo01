abcb7b5ceaed80d17bcd1c891957748a2c575fbb
huangzhimao1 <huangzhimao@jd.com>
2018-11-21 20:54:15 +0800

    测试@ModelAttribute在方法上的注解

--- src/main/java/com/fx/UploadDemo.java
+++ src/main/java/com/fx/UploadDemo.java
@@ -1,12 +1,19 @@
 package com.fx;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadDemo {
+
+    @ModelAttribute
+    public void populateModel(@RequestParam String name, Model model) {
+        model.addAttribute("attributeName", name);
+    }
 
     @RequestMapping("/demo/init.do")
     public String init(){
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,7 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <body>
 <h2>Hello World!</h2>
+<c:out value="${attributeName}"></c:out>
 <div>
     <form action="/demo/testupload.do" method="post" enctype="multipart/form-data">
         选择文件：<input type="file" name="multiFile">
